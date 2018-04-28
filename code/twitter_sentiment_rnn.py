17a0d7b4f60e2b0dac6bad9466f0414ed94d21a7
Fan-Luo <luofan813@gmail.com>
2018-04-28 01:21:46 -0700

    update rnn, 15th row

--- code/twitter_sentiment_rnn.py
+++ code/twitter_sentiment_rnn.py
@@ -13,23 +13,26 @@ from keras.utils import plot_model
 import pydot
 
 def read_data(filename):
-    labels = []
-    sentences = []
-    sentences_str = []
-    tags = []
     IDs = []
-
+    sentences_str = []
+    
+    char_str = []
+    sentences_words = []
+    tags = []
+    
+    max_word_len = 0
     max_tags_num = 0
     max_sentence_len = 0
-
-
+    
+    labels = []
+    
     with open(filename) as f:
         f.readline()
         for line in f:
-
             vals = line.strip().split('\t')
             ID = vals[0].strip()
             sentence_str = vals[1].strip()
+            
             sentence_str = sentence_str.replace(':(', "sad")
             sentence_str = sentence_str.replace('T_T', "sad")
             sentence_str = sentence_str.replace(':)', "joy")
@@ -37,64 +40,134 @@ def read_data(filename):
             sentence_str = sentence_str.replace('^^', "joy")
             sentence_str = sentence_str.replace(';-)', "cute")
             sentence_str = sentence_str.replace(':P', "blink")
-
-            # split at each punctuation, remove punctuation at the meanwhile
-            sentences_words = re.split(r'\\n| |%|\'|\"|,|:|;|!|=|\.|\(|\)|\$|\?|\*|\+|\-|\]|\[|\{|\}|\\|\/|\||\<|\>|\^|\`|\~', sentence_str)  
-
-            tag = [] 
+            sentence_str = sentence_str.replace('\\n', " ")
+            sentence_str = ' '.join(sentence_str.split())
+            
+            # split at each punctuation
+            split_words = re.split(r'(\\n|%|\'|\"|,|:|;|!|=|\.|\(|\)|\$|\?|\*|\+|\-|\]|\[|\{|\}|\\|\/|\||\<|\>|\^|\`|\~)', sentence_str)
+            sentence_words = []
+            for words in split_words:
+                sentence_words.extend(words.split())
+            
+            tag = []
             istag = 1
-            sentences_words.reverse()    # when words with # locates at the end of sentence, they are considered as tags
-
-            for i, word in enumerate(sentences_words):  
+            sentence_words.reverse()    # when words with # locates at the end of sentence, they are considered as tags
+            
+            i = 0
+            while i < len(sentence_words):
+                # for i, word in enumerate(sentence_words):
+                word = sentence_words[i]
+                
                 if len(word) == 0:
-                    sentences_words.remove(word)
-                    i -= 1
-
+                    sentence_words.remove(word)
+                
                 elif word[0] is '#' and istag == 1:    #tags
                     tag.append(word[1:])
-                    sentences_words.remove(word)
-                    i -= 1
-                    
-                elif word[0] is '#' and istag == 0:   #words in sentence
-                    sentences_words[i] = word[1:]
-
-                elif word[0] is '@':
-                    sentences_words[i] = "@NAME"      #someone's name
-                    istag = 0
-
-                elif any(char.isdigit() for char in word):
-                    sentences_words[i] = "xnumx"
-                    istag = 0
-
+                    sentence_words.remove(word)
+                
                 else:
-                    istag = 0 
+                    if word[0] is '#' and istag == 0:    #words in sentence
+                        sentence_words[i] = word[1:]
                     
-            sentences_words.reverse()
-            sentences.append(sentences_words)
-            sentences_str.append(sentence_str)
-            tags.append(tag)
-            labels.append(vals[2:13])
-            IDs.append(ID)
+                    elif word[0] is '@':
+                        sentence_words[i] = "@NID"       #someone's Network ID
+                        istag = 0
+                    
+                    elif word.isdigit():
+                        sentence_words[i] = "num"
+                        istag = 0
+                    
+                    elif any(char.isdigit() for char in word) and word.isalnum():
+                        sentence_words[i] = "xnumx"
+                        istag = 0
+                
+                    elif not word.isalnum():
+                        if len(word) > 1:
+                            l_idx = 0
+                            while l_idx < len(word):
+                                
+                                if not word[l_idx].isalnum() and word[l_idx] is not ' ':
+                                    if l_idx > 0 and word[l_idx] in word[:l_idx] and l_idx < len(word)-1:
+                                        word = word[:l_idx] + ' ' + word[l_idx+1:]
+                                    elif l_idx > 0 and word[l_idx] in word[:l_idx] and l_idx == len(word)-1:
+                                        word = word[:l_idx] + ' ' 
+                                    
+                                    # elif l_idx > 0 and word[l_idx] == word[l_idx - 1] and l_idx == len(word)-1:
+                                    #     word = word[:l_idx]
+                                    
+                                    elif l_idx > 0 and l_idx < len(word)-1:
+                                        word = word[:l_idx] + ' ' + word[l_idx] + ' ' + word[l_idx+1:]
+                                        l_idx += 1
+                                    elif l_idx > 0 and l_idx == len(word)-1:
+                                        word = word[:l_idx] + ' ' + word[l_idx]
+                                        l_idx += 1
+                                    elif l_idx == 0: 
+                                        word = word[l_idx] + ' ' + word[l_idx+1:]
+                        
+                                l_idx += 1
+                            
+                            word = ' '.join(word.split())
+                            word_split = word.split()
+                            word_split.reverse()
+
+                            if (i < len(sentence_words)-1):
+                                sentence_words = sentence_words[:i] + word_split + sentence_words[i+1:]
+                            else:
+                                sentence_words = sentence_words[:i] + word_split
+                            
+                            i -= 1
+    
+                        istag = 0
+
+                    else:
+                        istag = 0
+                      
+
+                    if len(sentence_words[i]) > max_word_len:
+                        max_word_len = len(sentence_words[i])
+
+                    i += 1
+            # if max_word_len > 70:
+            #     print (word)
+            #     sys.exit()
             
-       
+
+            IDs.append(ID)
+            sentences_str.append(sentence_str)
+            sentence_words.reverse()
+            sentences_words.append(sentence_words)
+            tags.append(tag)
+
+            labels.append(vals[2:13])
+            
             if len(tag) > max_tags_num:
-                max_tags_num = len(tag) 
-
-            if len(sentences_words) > max_sentence_len:
-                max_sentence_len = len(sentences_words)
-     
+                max_tags_num = len(tag)
+            
+            if len(sentence_words) > max_sentence_len:
+                max_sentence_len = len(sentence_words)
+                    
         labels = np.array(labels)
+             
+    # print('#########')           
+    # print(IDs)
+    # print(sentences_str)  
+    # print(sentences_words)  
+    # print(tags)        
+    # print(labels)  
+    # print(max_word_len)  
+    # print(max_sentence_len)  
+    # print(max_tags_num)  
 
-  
-    return IDs, sentences_str, sentences, tags, labels, max_tags_num, max_sentence_len
+    # sys.exit()
+    return IDs, sentences_str, sentences_words, tags, labels, max_word_len, max_sentence_len, max_tags_num
 
 
 class load_data():
     PAD = "@PADDING"
     OOV = "</s>"
-    NAME = "@NAME"
+    NID = "@NID" 
     OOV_ID = 0
-    NAME_ID = -1
+    NID_ID = -1
 
     def __init__(self, type='train'):
 
@@ -102,7 +175,7 @@ class load_data():
         # w2vfile = "vectors.goldbergdeps.txt"
 
         if type is 'train':
-            _, _, self.sentences_words, self.tags_words, self.labels, self.max_tags_num, self.max_sentence_len = read_data(dataset_file)
+            _, _, self.sentences_words, self.tags_words, self.labels, self.max_word_len, self.max_sentence_len, self.max_tags_num = read_data(dataset_file)
 
             self.word_vocab = Vocabulary()
             for word in chain.from_iterable(zip(*self.sentences_words)):
@@ -119,7 +192,7 @@ class load_data():
 
             maxlen_file = "maxlen_train.txt"
             with io.open(maxlen_file, 'w', encoding='utf8') as f:
-                f.write(str(self.max_sentence_len) + '\t' + str(self.max_tags_num))
+                f.write(str(self.max_word_len) + '\t' + str(self.max_sentence_len) + '\t' + str(self.max_tags_num))
 
         else:
 
@@ -129,12 +202,12 @@ class load_data():
             maxlen_file = "maxlen_train.txt"
             with io.open(maxlen_file, encoding='utf8') as f:
                 for line in f:
-                    [self.max_sentence_len, self.max_tags_num] = [int(v) for v in line.split('\t')]
+                    [self.max_word_len, self.max_sentence_len, self.max_tags_num] = [int(v) for v in line.split('\t')]
 
-            self.IDs, self.sentences_str, self.sentences_words, self.tags_words, self.labels, _, _ = read_data(dataset_file)
+            self.IDs, self.sentences_str, self.sentences_words, self.tags_words, self.labels, _, _, _ = read_data(dataset_file)
 
         self.OOV_ID = self.word_vocab.get_id(load_data.OOV)
-        self.NAME_ID = self.word_vocab.get_id(load_data.NAME)
+        self.NID_ID = self.word_vocab.get_id(load_data.NID)
 
     def pad_item(self, dataitem, type='sentence'):
         if (type is 'sentence'): 
@@ -176,13 +249,13 @@ def twitter_rnn(vocabulary_size: int, sentence_length: int, tag_num: int, n_outp
 
     # sentence
     model_sentence = Sequential()
-    model_sentence.add(Embedding(vocabulary_size, output_dim=512, input_length=sentence_length)) 
+    model_sentence.add(Embedding(vocabulary_size, output_dim=256, input_length=sentence_length)) 
     model_sentence.add(Bidirectional(GRU(128, return_sequences=True))) 
     model_sentence.add(Bidirectional(GRU(50)))  
 
     #tag
     model_tag = Sequential()
-    model_tag.add(Embedding(vocabulary_size, output_dim=512, input_length=tag_num)) 
+    model_tag.add(Embedding(vocabulary_size, output_dim=256, input_length=tag_num)) 
     model_tag.add(Flatten())  
     model_tag.add(Dense(100, activation='tanh')) 
 
@@ -193,7 +266,7 @@ def twitter_rnn(vocabulary_size: int, sentence_length: int, tag_num: int, n_outp
     model.compile(loss='binary_crossentropy', optimizer='Adam', metrics=['accuracy'])
 
     print(model.summary())
-    plot_model(model, show_shapes = True, to_file='rnn14.png')
+    plot_model(model, show_shapes = True, to_file='rnn9.png')
 
     kwargs = {'callbacks': [EarlyStopping(monitor='val_loss', min_delta=0.0001, patience=5, verbose=0, mode='auto')], 'batch_size': 32}
 
@@ -210,7 +283,7 @@ def twitter_cnn(vocabulary_size: int, n_inputs: int, n_outputs: int) -> Tuple[ke
 
     # sentence
     model = Sequential()
-    model.add(Embedding(vocabulary_size, output_dim=512, input_length=n_inputs)) 
+    model.add(Embedding(vocabulary_size, output_dim=256, input_length=n_inputs)) 
     model.add(Conv1D(100, 3, activation='tanh'))
     model.add(MaxPooling1D(pool_size=2))
     model.add(Conv1D(50, 3, activation='tanh')) 
@@ -247,10 +320,10 @@ def main():
         n_outputs = train_out.shape[1]
         n_inputs = train_concate.shape[1]
 
-        print('nums:')
-        print (sentence_len)
-        print (tag_n)
-        print (n_inputs)
+        # print('nums:')
+        # print (sentence_len)
+        # print (tag_n)
+        # print (n_inputs)
 
         dev_dataset = load_data('dev')
         dev_sentence, dev_tag = dev_dataset.get_input()
